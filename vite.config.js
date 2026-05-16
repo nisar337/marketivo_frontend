11471e673fafd6dfbfab0a194798ff368223346f
nisar337 <hopefull77778@gmail.com>
2026-05-16 22:29:44 +0500

    file sizes changes

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'router-vendor': ['react-router-dom'],
+          'axios-vendor': ['axios'],
+          'icons-vendor': ['react-icons'],
+        },
+      },
+    },
+  },
 })
